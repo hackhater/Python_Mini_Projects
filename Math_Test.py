a62ce0d6a75f22c76af1341fb03d910c8bb336f7
hackhater <hackhater98@gmail.com>
2022-07-30 19:52:25 +0630

    Math_Test code refactoring and some improvements

--- Math_Test.py
+++ Math_Test.py
@@ -1,7 +1,9 @@
 
 import random
+
 ope = '+-x÷'
 n = int(input('How many Math Questions do you want? \nyou:'))
+
 def Random_Ope():
     return random.choice(ope)
 
@@ -14,36 +16,26 @@ def Math_Ope(n,n1,n2,o1):
     score = 0
     if o1 == '+':
         right_ans = num1 + num2
-        user_ans = int(input(f'{n}. What is {n1} {o1} {n2} equal to? \nyou:'))
-        if user_ans == right_ans:
-            print('Correct!')
-            score += 1
     elif o1 == '-':
         right_ans = num1 - num2
-        user_ans = int(input(f'{n}. What is {n1} {o1} {n2} equal to? \nyou:'))
-        if user_ans == right_ans:
-            print('Correct!')
-            score +=1
     elif ope1 == 'x':
         right_ans = num1 * num2
-        user_ans = int(input(f'{n}. What is {n1} {o1} {n2} equal to? \nyou:'))
-        if user_ans == right_ans:
-            print('Correct!')
-            score +=1
     else:
-        right_ans = num1 / num2
-        user_ans = int(input(f'{n}. What is {n1} {o1} {n2} equal to? \nyou:'))
-        if user_ans == right_ans:
-            print('Correct!')
-            score +=1
-    answer_list.append(f'{n1} {o1} {n2} = {right_ans} | you: {user_ans}')
+        right_ans = round(num1 / num2, 2)
+    
+    user_ans = round(float(input(f'{n+1}. What is {n1} {o1} {n2} equal to? \nyou:')),2)
+    answer_list.append(f'{n+1}. {n1} {o1} {n2} = {right_ans} | you: {user_ans}')
+
+    if user_ans == right_ans:
+        print('Correct!')
+        score += 1
     return score
 
 for num in range(n):
     num1,num2 = random.randint(1,10),random.randint(1,10)
     ope1 = Random_Ope()
-    score += Math_Ope(n,num1,num2,ope1)
+    score += Math_Ope(num,num1,num2,ope1)
 
-print(f'Thanks for playing!\nYou got {score} out of {n} ({round(score/n)*100}%) correct.')
+print(f'Thanks for playing!\nYou got {score} out of {n} ({round(score/n, 1)*100}%) correct.')
 for a in answer_list:
     print(a)
