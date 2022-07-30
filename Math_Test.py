818ff31be8367dc545ab9132de32214c7fdda715
hackhater <hackhater98@gmail.com>
2022-07-30 20:15:43 +0630

    adding comments to Math_Test for user understanding

--- Math_Test.py
+++ Math_Test.py
@@ -2,18 +2,21 @@
 import random
 
 ope = '+-x÷'
-n = int(input('How many Math Questions do you want? \nyou:'))
+# user's input for number of question
+no_question = int(input('How many Math Questions do you want? \nyou:'))
 
+# function for Randomizing math operators
 def Random_Ope():
     return random.choice(ope)
-
+# list and score for final result
 answer_list = []
 score = 0
-
-def Math_Ope(n,n1,n2,o1):
+# function for Math Operations, Score counting and answer_list appending
+def Math_Operation(n,n1,n2,o1):
     right_ans = 0
     user_ans = 0
     score = 0
+    # Math Operation conditions
     if o1 == '+':
         right_ans = num1 + num2
     elif o1 == '-':
@@ -22,20 +25,24 @@ def Math_Ope(n,n1,n2,o1):
         right_ans = num1 * num2
     else:
         right_ans = round(num1 / num2, 2)
-    
+    # User's input for math answer
     user_ans = round(float(input(f'{n+1}. What is {n1} {o1} {n2} equal to? \nyou:')),2)
+    # Math questions and answers appending
     answer_list.append(f'{n+1}. {n1} {o1} {n2} = {right_ans} | you: {user_ans}')
-
+    # Score counting
     if user_ans == right_ans:
         print('Correct!')
         score += 1
+    # return score
     return score
 
-for num in range(n):
+# Actually, program excuation starts here
+# looping for the number of questions
+for no in range(no_question):
     num1,num2 = random.randint(1,10),random.randint(1,10)
     ope1 = Random_Ope()
-    score += Math_Ope(num,num1,num2,ope1)
-
-print(f'Thanks for playing!\nYou got {score} out of {n} ({round(score/n, 1)*100}%) correct.')
+    score += Math_Operation(no,num1,num2,ope1)
+# Final result output
+print(f'Thanks for playing!\nYou got {score} out of {no_question} ({round(score/no_question, 1)*100}%) correct!')
 for a in answer_list:
     print(a)
